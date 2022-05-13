746f48ec1314182a81340f174a1f07cb6822e417
RemiPoiroux <remi.poiroux7@orange.fr>
2022-05-13 10:26:54 +0200

    c'est un début

---
+++ src/include/personnages.h
@@ -0,0 +1,15 @@
+typedef struct perso
+{
+    //position
+    int px;
+    int py;
+    //int p.z; si jamais y a besoin
+
+    //vitesse
+    int vx;
+    int vy;
+    
+    int mass;
+
+    ColorRGB color;
+};
